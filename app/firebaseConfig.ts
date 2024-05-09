382437058a7f2cbdd1a449cdaf03b8bf479c6028
Gabe Wyatt <gabe@gabewyatt.com>
2024-05-09 15:42:56 -0700

    ci: add firebaseConfig

---
+++ app/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzQRI9qaoCkiE3FAI_Vr2w7Kwh_gJjeoE",
+  authDomain: "crosshare-times.firebaseapp.com",
+  projectId: "crosshare-times",
+  storageBucket: "crosshare-times.appspot.com",
+  messagingSenderId: "1093364351700",
+  appId: "1:1093364351700:web:f7e06a4413a10d8933f141"
+};
